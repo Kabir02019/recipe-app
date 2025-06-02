b7793919d5ef4f275877389cef71ed47844a5a66
Kabir Khanna <kabirkhanna@Kabirs-MacBook-Pro.local>
2025-06-01 20:55:31 -0400

    CI/CD test: updated app title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -78,7 +78,7 @@ const App = () => {
     <div className="app-container">
       <div className="header">
         <img src="/hero-image.jpg"></img>
-        <div className="title">My Recipe App</div>
+        <div className="title">My Recipe App CI/CD</div>
       </div>
       <div className="tabs">
         <h1
